5ef446c2165d2a63cd175ad457d7730302b5498b
thebest100100011 <anish.chilakala@gmail.com>
2026-04-03 09:40:34 -0400

    Add HiResTimer header file

--- include/iqos/timing/hires_timer.hpp
+++ include/iqos/timing/hires_timer.hpp
@@ -1,5 +1,25 @@
+#pragma once
+
 #include "vex.h"
 
-uint32_t startTime;
-double elapsedTime;
-bool isTimerRunning;
+namespace iqos {
+namespace timing {
+
+class HiResTimer {
+public:
+//Calling the functions
+    void start();
+    void stop();
+    void reset();
+    double getElapsedMs();
+
+private:
+    //Defining variables 
+    uint32_t startTime;
+    double elapsedTime;
+    bool isTimerRunning;
+};
+
+} // namespace timing
+} // namespace iqos
+
