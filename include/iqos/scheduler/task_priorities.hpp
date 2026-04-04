d3d1b15684a452f425d3c3a99a33d78649175653
thebest100100011 <anish.chilakala@gmail.com>
2026-04-04 11:00:49 -0400

    Fix c_cpp_properties.json include paths for all platforms

--- include/iqos/scheduler/task_priorities.hpp
+++ include/iqos/scheduler/task_priorities.hpp
@@ -2,11 +2,13 @@
 
 #include "vex.h"
 
-namespace iqos::scheduler {
+namespace iqos {
+namespace scheduler {
     enum class TaskPriority {
         LOW = 0,
         MEDIUM = 1,
         HIGH = 2
     };
 
-}
+} // namespace scheduler
+} // namespace iqos
