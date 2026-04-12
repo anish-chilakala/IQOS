3f1d6238f8eafefd38c465421eb2da69dd84dcd7
thebest100100011 <anish.chilakala@gmail.com>
2026-04-12 09:21:26 -0400

    Update ErrorCodes module

--- include/iqos/errors/error_codes.hpp
+++ include/iqos/errors/error_codes.hpp
@@ -4,19 +4,45 @@
 
 namespace iqos {
 namespace errors {
-    enum class ErrorCodes {
-        //Define enum of all possible error codes:
+    enum class ErrorCode {
+    // No error
     NONE = 0,
+
+    // Motor errors
     MOTOR_STALL = 1,
     MOTOR_OVERHEAT = 2,
     MOTOR_DISCONNECT = 3,
-    SENSOR_DISCONNECT = 4,
-    SENSOR_OUT_OF_RANGE = 5,
-    TASK_FROZEN = 6,
-    LOW_BATTERY = 7,
-    CONFIG_LOAD_FAILED = 8,
-    STORAGE_WRITE_FAILED = 9
-    };
+    MOTOR_OVERCURRENT = 4,
+
+    // Sensor errors
+    SENSOR_DISCONNECT = 5,
+    SENSOR_OUT_OF_RANGE = 6,
+    SENSOR_CALIBRATION_FAILED = 7,
+
+    // Memory errors
+    STORAGE_READ_FAILED = 8,
+    STORAGE_WRITE_FAILED = 9,
+    CONFIG_LOAD_FAILED = 10,
+    CONFIG_SAVE_FAILED = 11,
+    OUT_OF_MEMORY = 12,
+
+    // System errors
+    BOOT_FAILED = 13,
+    WATCHDOG_TIMEOUT = 14,
+    MUTEX_TIMEOUT = 15,
+
+    // Controller errors
+    CONTROLLER_DISCONNECT = 16,
+
+    // Task errors
+    TASK_FROZEN = 17,
+    TASK_CREATE_FAILED = 18,
+
+    // Power errors
+    LOW_BATTERY = 19,
+    CRITICAL_BATTERY = 20,
+    THERMAL_THROTTLE = 21
+};
 
 } // namespace errors
 } // namespace iqos
