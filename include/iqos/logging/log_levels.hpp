63d407c354904421e099585a78bf4be555cc2917
thebest100100011 <anish.chilakala@gmail.com>
2026-04-12 12:15:36 -0400

    Add LogLevel module

---
+++ include/iqos/logging/log_levels.hpp
@@ -0,0 +1,14 @@
+#pragma once
+
+#include "vex.h"
+
+namespace iqos {
+namespace logging {
+    enum class LogLevel {
+DEBUG = 0,
+INFO = 1,
+WARNING = 2,
+ERROR = 3
+    };
+} // namespace logging
+} // namespace iqos
